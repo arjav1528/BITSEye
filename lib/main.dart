7148087df3763195c88d72b65d3a5ce99f61d7af
Arjav Patel <arjav1528@gmail.com>
2024-12-30 15:35:44 +0530

    Fixed the Issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:newproject2/custom_theme.dart';
 import 'screens/list.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      textTheme: CustomTheme.darkTextTheme,
+    ),
     debugShowCheckedModeBanner: false,
     home: CardList(),
   ));
